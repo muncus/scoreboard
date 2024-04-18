70c738a1793a923902379e10ef363714ba5e7613
Marc Dougherty <muncus@users.noreply.github.com>
2024-04-18 15:00:02 -0700

    initial commit.
    works with input from stdin.

---
+++ README.md
@@ -0,0 +1,7 @@
+# scoreboard tool
+
+Scoreboard creates a visualization of incoming JSON objects (or HTTP responses).
+
+It can be useful when altering the behavior of a service to illustrate the
+effects. Think of a service that is producing 500 errors, and when fixed, the
+responses shift to 200s.
---
+++ main.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log/slog"
+	"os"
+	"sort"
+	"time"
+
+	"github.com/charmbracelet/bubbles/progress"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+// TODO: add a 'topN' int flag, condense others into "Other" bucket.
+
+type Score struct {
+	Name   string
+	Points int
+}
+
+type Model struct {
+	total     int
+	scores    []Score
+	scoreChan chan Score
+	width     int
+}
+
+var fetchUrl = flag.String("url", "", "url to fetch content from")
+var jsonField = flag.String("json", "", "top-level json field to read from body")
+
+func (m Model) Init() tea.Cmd {
+	return tea.Batch(
+		sendEvent(m.scoreChan),
+		readEvent(m.scoreChan),
+	)
+}
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	case tea.WindowSizeMsg:
+		m.width = msg.Width
+
+	// Is it a key press?
+	case tea.KeyMsg:
+
+		// Cool, what was the actual key pressed?
+		switch msg.String() {
+
+		// These keys should exit the program.
+		case "ctrl+c", "q":
+			return m, tea.Quit
+
+			// any other key scores points
+			// default:
+			// 	return m, func() tea.Msg {
+			// 		return Score{Name: msg.String(), Points: 1}
+			// 	}
+		}
+	case Score:
+		m.total += msg.Points
+		for i, s := range m.scores {
+			if s.Name == msg.Name {
+				m.scores[i].Points += msg.Points
+				return m, readEvent(m.scoreChan)
+			}
+		}
+		m.scores = append(m.scores, msg)
+		return m, readEvent(m.scoreChan)
+
+	default:
+		fmt.Printf("not sure about: %#v\n", msg)
+	}
+	return m, nil
+
+}
+func readEvent(ch chan Score) tea.Cmd {
+	return func() tea.Msg {
+		return <-ch
+	}
+}
+func sendEvent(ch chan Score) tea.Cmd {
+	return func() tea.Msg {
+		for {
+			if inDecoder.More() {
+				var m map[string]interface{}
+				err := inDecoder.Decode(&m)
+				if err != nil {
+					slog.Error("Failed to decode message", "error", err)
+				}
+				time.Sleep(600 * time.Millisecond)
+				ch <- Score{Name: fmt.Sprint(m["name"]), Points: 1}
+			}
+		}
+	}
+}
+func (m Model) View() string {
+	rval := "\n"
+
+	if m.total == 0 {
+		return "no data yet\n"
+	}
+
+	sort.Slice(m.scores, func(i, j int) bool {
+		return m.scores[i].Points > m.scores[j].Points
+	})
+
+	for _, s := range m.scores {
+		rval += fmt.Sprintf("Bucket: %s\n", s.Name)
+		p := float64(s.Points) / float64(m.total)
+		rval += progress.New(progress.WithWidth(m.width)).ViewAs(p)
+		rval += "\n\n"
+	}
+	return rval
+}
+
+func initialModel() Model {
+
+	m := Model{
+		total:     0,
+		scores:    make([]Score, 0),
+		scoreChan: make(chan Score),
+	}
+	return m
+}
+
+var inDecoder *json.Decoder
+
+func main() {
+	flag.Parse()
+	inDecoder = json.NewDecoder(os.Stdin)
+	p := tea.NewProgram(initialModel(), tea.WithInput(nil), tea.WithAltScreen())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
+}
